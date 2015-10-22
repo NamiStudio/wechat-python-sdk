3132bbf501437e19ab6aa72126b030d01fa5337a
kehao <kehao95@gmail.com>
2015-10-22 11:52:50 +0800

    fixed issue #22 : import StringIO ERROR

--- wechat_sdk/basic.py
+++ wechat_sdk/basic.py
@@ -5,7 +5,10 @@ import requests
 import time
 import json
 import cgi
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 from xml.dom import minidom
 
